a8f91426dda0d025f827e38e302b7afadc51ce20
markmalko <markmalkoweb@gmail.com>
2018-11-07 22:09:07 +0300

    continue

---
+++ reboot/image-preview-download/images/form.php
@@ -0,0 +1,10 @@
+<?php include_once('processForm.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie-edge">
+    <title>Image Preview and Upload</title>
+</head>
+</html>
